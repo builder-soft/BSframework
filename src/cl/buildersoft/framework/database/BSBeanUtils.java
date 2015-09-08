7f9691929036566b0b5999e28920c74886af1c01
Claudio Moscoso <claudio.moscoso@gmail.com>
2015-09-08 00:01:07 -0300

    Organizacion de importacion

--- src/cl/buildersoft/framework/database/BSBeanUtils.java
+++ src/cl/buildersoft/framework/database/BSBeanUtils.java
@@ -1,6 +1,5 @@
 package cl.buildersoft.framework.database;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;

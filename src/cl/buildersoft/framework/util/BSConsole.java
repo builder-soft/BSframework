f575227af992326abccf8ad157471f7e05b0679e
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-06-10 23:22:42 -0400

    Inicio de aplicacion de console para escuchar a los relojes

--- src/cl/buildersoft/framework/util/BSConsole.java
+++ src/cl/buildersoft/framework/util/BSConsole.java
@@ -1,5 +1,7 @@
 package cl.buildersoft.framework.util;
 
+import java.util.Scanner;
+
 public class BSConsole {
 	public static void println(String m) {
 		System.out.println(m);
@@ -8,6 +10,17 @@ public class BSConsole {
 	public static void print(String m) {
 		System.out.print(m);
 	}
-	
-	
+
+	public static String readString() {
+		return readString(null);
+	}
+
+	public static String readString(String msg) {
+		Scanner inputReader = new Scanner(System.in);
+		if (msg != null) {
+			print(msg);
+		}
+		String input = inputReader.nextLine();
+		return input;
+	}
 }

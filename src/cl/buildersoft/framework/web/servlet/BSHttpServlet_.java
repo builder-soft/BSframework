a992ff917c278fff89e2d1c2cef93b5bf8d2420d
Claudio Moscoso <claudio.moscoso@gmail.com>
2016-07-07 18:05:06 -0400

    Mejoras para el manejo del log4j2

--- src/cl/buildersoft/framework/web/servlet/BSHttpServlet_.java
+++ src/cl/buildersoft/framework/web/servlet/BSHttpServlet_.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.sql.Connection;
